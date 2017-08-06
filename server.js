bea026465b2ef6d1703d684df8b75c452c84f92f
Luo Rock <luorock@LuodeMacBook-Air.local>
2017-08-06 16:40:27 -0700

    installed react, properlrly load files in html

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-var app = require('express')();
+
+var Express = require('express');
+var app = Express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
@@ -6,6 +8,8 @@ var port = process.env.PORT || 3000;
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+
+app.use("/public", Express.static(__dirname + '/public'));
 
 io.on('connection', function(socket){
   socket.on('messages', function(msg){
